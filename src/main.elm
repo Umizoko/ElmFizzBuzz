d110a4eb170ec8ab9856f01b7ff01897582547eb
umi <0123mark3210@gmail.com>
2019-01-03 21:53:19 +0900

    fix input_datas

--- src/main.elm
+++ src/main.elm
@@ -1,7 +1,7 @@
 import Html exposing (..)
 import Debug exposing (..)
 
-input_datas = [1,2,3,4,5]
+input_datas = List.map toFloat (List.range 1 30)
 
 restZero index number = 
     toFloat (floor index // number)
@@ -10,12 +10,9 @@ judgeFizzBuzz index =
     if index / 15 == restZero index 15 then "Fizz Buzz | "
     else if index / 5 == restZero index 5 then "Buzz | "
     else if index / 3 == restZero index 3 then "Fizz | "
-    else "No Fizz Buzz | "
+    else toString  index ++ " | "
 
 fizzbuzz = List.map judgeFizzBuzz input_datas
-
-title = h1 [] [text "Elm Fizz Buzz Demo"]
-world = p [] [text "test"]
 
 main = 
     -- div [] [title, world]
