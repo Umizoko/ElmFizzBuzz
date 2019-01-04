75020fea3571d549453e30cfa5ea2a8705bde086
umi <0123mark3210@gmail.com>
2019-01-04 13:36:35 +0900

    追加 型注釈

--- src/main.elm
+++ src/main.elm
@@ -1,15 +1,18 @@
 import Html exposing (..)
 import Debug exposing (..)
 
-input_datas = List.map toFloat (List.range 1 30)
+input_datas : List Int
+input_datas = List.range 1 30
 
+restZero : Int -> Int -> Float
 restZero index number = 
-    toFloat (floor index // number)
+    toFloat ( index // number )
 
+judgeFizzBuzz : Int -> String
 judgeFizzBuzz index = 
-    if index / 15 == restZero index 15 then "Fizz Buzz | "
-    else if index / 5 == restZero index 5 then "Buzz | "
-    else if index / 3 == restZero index 3 then "Fizz | "
+    if toFloat index / 15 == restZero index 15 then "Fizz Buzz | "
+    else if toFloat index / 5 == restZero index 5 then "Buzz | "
+    else if toFloat index / 3 == restZero index 3 then "Fizz | "
     else toString  index ++ " | "
 
 fizzbuzz = List.map judgeFizzBuzz input_datas
